e97b037eaecefb0ff161a5d1f3aa9f58f0698d51
bayusuryowibowo <bayusuryowibowo@gmail.com>
2023-12-25 16:17:32 +0700

    feat: implement Swagger
    Signed-off-by: bayusuryowibowo <bayusuryowibowo@gmail.com>

--- src/main/java/QAEngineer/ShareMySight/exception/GlobalException.java
+++ src/main/java/QAEngineer/ShareMySight/exception/GlobalException.java
@@ -1,24 +1,32 @@
 package QAEngineer.ShareMySight.exception;
 
-import QAEngineer.ShareMySight.model.StandardResponse;
+import QAEngineer.ShareMySight.model.response.ErrorMessageResponse;
+import QAEngineer.ShareMySight.model.response.StandardResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
-@ControllerAdvice
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class GlobalException {
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<StandardResponse<Object>> handleCustomException(CustomException ex) {
         StandardResponse<Object> response = StandardResponse.<Object>builder()
                 .status(false)
-                .code(ex.getCode())
+                .code(ex.getCode().value())
                 .message(ex.getMessage())
                 .data(null)
                 .build();
 
-        return new ResponseEntity<>(response, HttpStatus.valueOf(ex.getCode()));
+        return new ResponseEntity<>(response, HttpStatus.valueOf(ex.getCode().value()));
     }
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
@@ -43,5 +51,30 @@ public class GlobalException {
                 .build();
 
         return new ResponseEntity<>(response, HttpStatus.valueOf(400));
+    }
+    
+    // Setiap argument yang ada di body json jika terkena validasi, maka semuanya dilempar balik dengan mapping
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> errorMap = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(fieldError -> {
+            errorMap.put(fieldError.getField(), fieldError.getDefaultMessage());
+        });
+        return ResponseEntity.badRequest().body(errorMap);
+    }
+    
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ErrorMessageResponse> handleBadCredentialsException(BadCredentialsException ex) {
+        return ResponseEntity
+          .status(HttpStatus.BAD_REQUEST)
+          .body(ErrorMessageResponse.builder().message("Invalid email or password").build());
+    }
+    
+    @ExceptionHandler(DisabledException.class)
+    public ResponseEntity<ErrorMessageResponse> handleDisabledException(DisabledException ex) {
+        return ResponseEntity
+          .status(HttpStatus.FORBIDDEN)
+          .body(ErrorMessageResponse.builder().message(ex.getMessage()).build());
     }
 }
--- src/main/java/QAEngineer/ShareMySight/model/LoginRequest.java
+++ src/main/java/QAEngineer/ShareMySight/model/LoginRequest.java
@@ -1,5 +1,6 @@
 package QAEngineer.ShareMySight.model;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,6 +11,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoginRequest {
+    @NotBlank(message = "Email is required")
+    @NotEmpty(message = "Email is required")
+    @NotNull(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String email;
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, max = 50, message = "Password should be between 8 and 50 characters")
     private String password;
 }
