25421506a1478c976c58a13d8fa6c6da087290c3
bayusuryowibowo <bayusuryowibowo@gmail.com>
2024-01-07 00:51:20 +0700

    fix: exclude socket.io from Dispatcher
    Signed-off-by: bayusuryowibowo <bayusuryowibowo@gmail.com>

--- src/main/java/QAEngineer/ShareMySight/config/CorsConfig.java
+++ src/main/java/QAEngineer/ShareMySight/config/CorsConfig.java
@@ -3,6 +3,7 @@ package QAEngineer.ShareMySight.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -11,5 +12,10 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins( "http://localhost:3000").allowCredentials(true);
+    }
+    
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.setPatternParser(new CustomPathPatternParser());
     }
 }
